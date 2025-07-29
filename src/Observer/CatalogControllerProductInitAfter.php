85014db12a473929d58d303d093359a6be2fb5ab
Vitaliy Dmytruk <vitaliy@swissuplabs.com>
2025-07-30 00:24:28 +0300

    Add observer for catalog controller product initialization to handle layout updates

---
+++ src/Observer/CatalogControllerProductInitAfter.php
@@ -0,0 +1,39 @@
+<?php
+namespace Hyva\SwissupEasytabs\Observer;
+
+use Magento\Framework\App\ViewInterface;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Hyva\Theme\Service\CurrentTheme;
+
+class CatalogControllerProductInitAfter implements ObserverInterface
+{
+    private ViewInterface $view;
+    private CurrentTheme $currentThemeService;
+
+    public function __construct(
+        ViewInterface $view,
+        CurrentTheme $currentThemeService
+    ) {
+        $this->view = $view;
+        $this->currentThemeService = $currentThemeService;
+    }
+
+    /**
+     * Execute observer
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        $product = $observer->getEvent()->getProduct();
+        $controller = $observer->getEvent()->getControllerAction();
+        if ($product
+            && $controller instanceof \Swissup\Easytabs\Controller\Index\Index
+            && $this->currentThemeService->isHyva()
+        ) {
+            $this->view->getLayout()->getUpdate()->addHandle('catalog_product_view');
+        }
+    }
+}
